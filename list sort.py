75647b9fec23ae9b423b9989b288f3a4cf5f3572
amirreza <80186568+baamirreza@users.noreply.github.com>
2021-08-07 14:18:15 +0430

    Update list sort.py

--- list sort.py
+++ list sort.py
@@ -11,10 +11,10 @@ while True:
 
 print("your number is" , number_list)
 
-c=0
+
 for i in range(len(number_list)-1) :
     if number_list[i] > number_list [i+1]:
-        c =+ 1
+        
         print("list is sort")
 
     else :
